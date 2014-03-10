395f05bf5637a4cbd2878443975218a634317d64
adsahay <adi@sahay.co>
2014-03-10 18:05:08 +0530

    virtualenv lines

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,9 @@
 easy_install SpoofMAC
+
+# virtualenv stuff
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/apps
+source /usr/local/bin/virtualenvwrapper.sh
+
+# this helps compile stuff that breaks otherwise
+export ARCHFLAGS="-arch i386 -arch x86_64"
