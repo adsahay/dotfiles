b0460d50ca75a38822d94ef0a15276fda29e33d4
adsahay <adi@sahay.co>
2014-03-11 12:11:31 +0530

    Fixed powerline. More vim (python) edits

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,10 +1,3 @@
 # ref: https://github.com/carlcarl/powerline-zsh
-
-function _update_ps1()
-{
-  export PROMPT="$(powerline-zsh.py $?)"
-}
-precmd()
-{
-  _update_ps1
-}
+# ref: https://github.com/Lokaltog/powerline/issues/39
+source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
