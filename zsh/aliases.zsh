618949b1d1030c12858574dda76a0c0d54be26c1
adsahay <adi@sahay.co>
2014-04-23 04:01:34 +0530

    Improvements to powerline prompt

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+alias bower='noglob bower'
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,12 @@
 # ref: https://github.com/carlcarl/powerline-zsh
 # ref: https://github.com/Lokaltog/powerline/issues/39
-source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+# source /usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
+#
+function _update_ps1()
+{
+  export PROMPT="$(~/.dotfiles/script/powerline-zsh.py $?)"
+}
+precmd()
+{
+  _update_ps1
+}
